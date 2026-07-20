02345d08198a3e45747f10339bb21c94fb9abd36
Arjun Shukla-Degen <arjunshukladegen@gmail.com>
2026-07-19 18:59:55 -0700

    Keep forgetting to commit... did most of the work now

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 // Create a 16x16 grid of square divs.
 
-console.log("Hello, world!");
+// Select container for the grid
+const container = document.querySelector("#grid-container");
+
+// Testing tool (for console.log)
+let counter = 0;
+let gridSize = 16;
+
+// For loop, rows and cols
+for (let rows = 0; rows < 16; rows++) {
+  let rowDiv = document.createElement("div");
+  rowDiv.className = "row-div";
+  container.append(rowDiv);
+
+  for (let col = 0; col < 16; col++) {
+    let squareDiv = document.createElement("div");
+    squareDiv.className = "box-div";
+    counter = counter + 1;
+    rowDiv.append(squareDiv);
+    console.log(`(COL) Box #${counter} completed`);
+  }
+}
+
+const allBoxes = document.querySelectorAll(".box-div");
+allBoxes.forEach((div) => {
+  div.addEventListener("mouseenter", function (e) {
+    div.style.backgroundColor = "#000000";
+  });
+});
