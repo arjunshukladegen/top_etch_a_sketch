8dd2f6438eb47b7c1395126be750d85bfc19171f
Arjun Shukla-Degen <arjunshukladegen@gmail.com>
2026-07-19 21:23:06 -0700

    Finished up now with working input for size and 7 unique color themes

--- script.js
+++ script.js
@@ -20,6 +20,76 @@ const rainbowTheme = {
   10: "#F15BB5",
 };
 
+// Colors
+const pastelTheme = {
+  1: "#F7A9A8",
+  2: "#FBC99D",
+  3: "#FDF0A8",
+  4: "#B8E0C8",
+  5: "#A0D8D3",
+  6: "#ABC7E6",
+  7: "#C9B6E4",
+  8: "#E4B4D4",
+  9: "#D6C3A9",
+  10: "#F3E9DD",
+};
+
+// Colors
+const forestTheme = {
+  1: "#1B3A2F",
+  2: "#3B6B4A",
+  3: "#7A9B6E",
+  4: "#4A3423",
+  5: "#7B5E3B",
+  6: "#B89968",
+  7: "#E4D9BC",
+  8: "#EFE8D6",
+  9: "#C9A227",
+  10: "#8C2F39",
+};
+
+// Colors
+const oceanTheme = {
+  1: "#0A2E4D",
+  2: "#14507A",
+  3: "#3A7CA5",
+  4: "#0D6E6E",
+  5: "#35A29F",
+  6: "#7FD1C7",
+  7: "#B8E6DF",
+  8: "#E6CE9C",
+  9: "#F2E6CC",
+  10: "#E76F51",
+};
+
+// Colors
+const twilightTheme = {
+  1: "#2E1A47",
+  2: "#55357A",
+  3: "#8264B0",
+  4: "#7A3B5E",
+  5: "#B05A82",
+  6: "#D98BAE",
+  7: "#3B4A6B",
+  8: "#6B7CA0",
+  9: "#E0B14E",
+  10: "#E7D9F0",
+};
+
+// Colors
+const retroTheme = {
+  1: "#8C3B1E",
+  2: "#C05A2E",
+  3: "#E08A4B",
+  4: "#A8781E",
+  5: "#D4A017",
+  6: "#E8C35A",
+  7: "#1E5C5A",
+  8: "#3E8E88",
+  9: "#EFE0C0",
+  10: "#6B8E23",
+};
+
 function randomHex() {
   hexCode = "#";
 
@@ -58,6 +128,16 @@ function pickColor(colorMode) {
     return rainbowTheme[randomColor()];
   } else if (colorMode === "random") {
     return randomHex();
+  } else if (colorMode === "pastel") {
+    return pastelTheme[randomColor()];
+  } else if (colorMode === "ocean") {
+    return oceanTheme[randomColor()];
+  } else if (colorMode === "retro") {
+    return retroTheme[randomColor()];
+  } else if (colorMode === "twilight") {
+    return twilightTheme[randomColor()];
+  } else if (colorMode === "forest") {
+    return forestTheme[randomColor()];
   } else {
     return "#000000";
   }
